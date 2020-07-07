0479dd77e5dae97ab42d93ff7bddc9315a641c29
Evandro Ribeiro <ribeiroevandro@live.com>
2020-07-07 15:06:36 -0300

    chore: configure environment variables

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/noderest", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
